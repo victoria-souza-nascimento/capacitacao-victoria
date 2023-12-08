801fd313e6f6d857a5153fb0417b484494cacf50
Victoria Souza Nascimento <vicsouza19@icloud.com>
2023-12-08 16:08:29 -0300

    requisições HTTPS

--- client/index.js
+++ client/index.js
@@ -1,8 +1,51 @@
-let cep = 36013080
-let endpoint = 'https://viacep.com.br/ws/36013080/json/';
-let response = fetch(endpoint, {
-    method: "GET"
+let endpoint = 'http://localhost:3000/User';
+let user = {
+    username: 'user',
+    password: '2324'
+}
+fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {"Content-type": "application/json;charset=UTF-8"}
 })
 .then(async (res) => {
     console.log(await res.json());
+    fetch(endpoint, {
+        method: "GET"
+    })
+    .then(async (res) => {
+        console.log(await res.json());
+        user = {
+            username: 'user',
+            password: '443321'   
+        }
+        fetch(endpoint, {
+            method: "PUT",
+            body: JSON.stringify(user),
+            headers: {"Content-type":"application/json; charset=UTF-8"}
+        }).then(async (res) => {
+            fetch(endpoint, {
+                method: "GET"
+            })
+            .then(async (res) => {
+                console.log(await res.json());
+                user= {
+                    username: 'user'
+                }
+                fetch(endpoint, {
+                    method: "DELETE",
+                    body: JSON.stringify(user),
+                    headers: {"Content-type":"application/json; charset=UTF-8"}
+                }).then(async (res) => {
+                    fetch(endpoint, {
+                        method: "GET"
+                    })
+                    .then(async (res) => {
+                        console.log(await res.json());
+                    });
+                });
+            });
+        });
+    
+    });
 });
--- src/server.ts
+++ src/server.ts
@@ -10,19 +10,53 @@ app.use(express.json());
 
 app.post('/User', (req, res) => {
     let { username, password } = req.body;
+    if(!username || !password) return res.status(400).send({ error: 'Username and Password needed!'});
     let user: User = {
         username: username,
         password: password
     };
+    if(USERS.find( item => item.username === user.username)){
+        return res.status(400).send({ error: 'Username already in use!'});
+    }
 
     USERS.push(user)
-    res.status(201).send(user);
-})
-;
+    return res.status(201).send(user);
+});
+app.put('/User', (req, res) => {
+    let { username, password } = req.body;
+
+    let user: User | undefined = USERS.find( item => item.username === username);
+
+    if(user){
+        let newUser: User = {
+            username: username,
+            password: password
+        };
+
+        let index = USERS.findIndex( item => item.username === newUser.username);
+        USERS[index] = newUser;
+        return res.sendStatus(204);
+    }
+
+    return res.sendStatus(400);
+});
+
+app.delete('/User', (req, res) => {
+    let { username } = req.body;
+
+    let user: User | undefined = USERS.find( item => item.username === username);
+
+    if(user){
+        let index = USERS.findIndex( item => item.username === username);
+        USERS.splice(index,1);
+        return res.sendStatus(204);
+    }
+
+    return res.status(400).send('User does not exist!');
+});
 
 app.get('/User', (req, res) => {
-    res.status(200).send(USERS);
-}
-)
+    return res.status(200).send(USERS);
+});
 
 app.listen(PORT, () => console.log('Server running on PORT ${PORT}'));
